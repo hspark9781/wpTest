f227596726b13661e77e14e7e6119b5a1ae8a04c
hsprak9781 <hspark9781@gmail.com>
2023-10-05 15:51:34 +0900

    [Webpack]20231005

--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
 import './styles.css';
-import rabbit from './rabbit.png';
+import bg from './background.png';
+
+let env;
+
+// console.log(pw);
+if(process.env.NODE_ENV === 'development') {
+    env = dev;
+} else {
+    env = pro;
+}
+
+// console.log(env);
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.body.innerHTML = `<img src="${rabbit}"/>`;
+    document.body.innerHTML = `<img src="${bg}"/>`;
 })
